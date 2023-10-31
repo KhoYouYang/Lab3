949f3f7f0a3f225b3ca1b9b9cbb9d00b5c7b577c
YouYang <YOUYANG.22@ichat.sp.edu.sg>
2023-10-31 13:35:55 +0800

    Adding Test_bmi.py

---
+++ Test_bmi.py
@@ -0,0 +1,20 @@
+import Lab2.bmi
+import Lab2.bmi as bmi
+
+print ("Test_bmi")
+
+def test_bmi_normal_weight():
+    inputWeight = 57
+    inputHeight = 1.73
+    result = Lab2.bmi.calculate_bmi(inputHeight, inputWeight)
+    assert(result == 0)
+def test_bmi_over_weight():
+    inputWeight = 110
+    inputHeight = 1.73
+    result = Lab2.bmi.calculate_bmi(inputHeight, inputWeight)
+    assert (result == 1)
+def test_bmi_under_weight():
+    inputWeight = 20
+    inputHeight = 1.73
+    result = Lab2.bmi.calculate_bmi(inputHeight, inputWeight)
+    assert (result == -1)
