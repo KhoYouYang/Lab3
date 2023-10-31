77dd51a103314a769f2eff938d043996198992d4
YouYang <YOUYANG.22@ichat.sp.edu.sg>
2023-10-31 14:27:25 +0800

    updating Test_bmi.py

--- Test_bmi.py
+++ Test_bmi.py
@@ -1,4 +1,3 @@
-import Lab2.bmi
 import Lab2.bmi as bmi
 
 print ("Test_bmi")
@@ -6,15 +5,15 @@ print ("Test_bmi")
 def test_bmi_normal_weight():
     inputWeight = 57
     inputHeight = 1.73
-    result = Lab2.bmi.calculate_bmi(inputHeight, inputWeight)
+    result = bmi.calculate_bmi(inputHeight, inputWeight)
     assert(result == 0)
 def test_bmi_over_weight():
     inputWeight = 110
     inputHeight = 1.73
-    result = Lab2.bmi.calculate_bmi(inputHeight, inputWeight)
+    result = bmi.calculate_bmi(inputHeight, inputWeight)
     assert (result == 1)
 def test_bmi_under_weight():
     inputWeight = 20
     inputHeight = 1.73
-    result = Lab2.bmi.calculate_bmi(inputHeight, inputWeight)
+    result = bmi.calculate_bmi(inputHeight, inputWeight)
     assert (result == -1)
